eda5dc2d7e789e4517470ad57b259a70143a7b72
petrak@ <petrathekat@gmail.com>
2023-01-07 18:08:53 -0600

    hexcasting real

--- src/puzzle/checker.rs
+++ src/puzzle/checker.rs
@@ -1,12 +1,17 @@
+use ahash::AHashMap;
 use hex2d::{Angle, Coordinate, Direction};
+use itertools::Itertools;
+
+use crate::geom::EdgePos;
 
 use super::{Board, Puzzle};
 
 impl Board {
     pub fn is_solved(&self, puzzle: &Puzzle) -> Result<(), FailureReason> {
-        if self.find_euler_path().is_none() {
-            return Err(FailureReason::NotSinglePass);
-        }
+        let Some(euler) = self.find_euler_path() else {
+            return Err(FailureReason::NotSinglePass)
+        };
+        println!("{:?}", euler);
 
         // for each flank of the level
         for (marks, dir) in puzzle.marks.iter().zip([
@@ -94,9 +99,65 @@ impl Board {
         Ok(())
     }
 
+    /// https://github.com/gamma-delta/HexMod/blob/main/Common/src/main/java/at/petrak/hexcasting/api/spell/math/EulerPathFinder.kt
     pub fn find_euler_path(&self) -> Option<Vec<Coordinate>> {
-        // TODO
-        Some(Vec::new())
+        let mut graph = make_graph(self);
+
+        let odd_nodes = graph
+            .iter()
+            .filter_map(|(coord, edges)| {
+                if edges.count_ones() % 2 == 1 {
+                    Some(*coord)
+                } else {
+                    None
+                }
+            })
+            .collect_vec();
+        let mut current = match odd_nodes.len() {
+            0 => *graph.keys().next().unwrap(),
+            2 => odd_nodes[0],
+            _ => return None,
+        };
+
+        let mut stack = Vec::new();
+        let mut out = Vec::new();
+        // hacking a do-while loop, sorry
+        while {
+            println!("at {:?}", current);
+            let edges = graph.get_mut(&current).unwrap();
+            println!("edges {:06b}", *edges);
+            if *edges == 0 {
+                out.push(current);
+                current = stack.pop().unwrap();
+            } else {
+                stack.push(current);
+
+                let mut a_dir = None;
+                'dirs: for &dir in Direction::all() {
+                    let mask = 1 << dir as u8;
+                    if (*edges & mask) != 0 {
+                        a_dir = Some(dir);
+                        break 'dirs;
+                    }
+                }
+                let burn_dir = a_dir.unwrap();
+
+                *edges &= !(1 << burn_dir as u8);
+                if let Some(facing) = graph.get_mut(&(current + burn_dir)) {
+                    *facing &= !(1 << (burn_dir + Angle::Back) as u8);
+                }
+                current = current + burn_dir;
+            }
+
+            let graph_ok = match graph.get(&current) {
+                Some(it) => *it != 0,
+                None => false,
+            };
+            graph_ok || !stack.is_empty()
+        } {}
+        out.push(current);
+
+        Some(out)
     }
 }
 
@@ -104,4 +165,19 @@ impl Board {
 pub enum FailureReason {
     NotSinglePass,
     CountFailed,
+}
+
+fn make_graph(board: &Board) -> AHashMap<Coordinate, u8> {
+    let mut out = AHashMap::new();
+    for &coord in board.paths.keys() {
+        for &dir in Direction::all() {
+            if board.get_path(EdgePos::new(coord, dir)) == Some(true) {
+                *out.entry(coord).or_default() |= 1 << dir as u8;
+                *out.entry(coord + dir).or_default() |=
+                    1 << (dir + Angle::Back) as u8;
+            }
+        }
+    }
+
+    out
 }
